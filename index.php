8a509011948ba6bbc3aa74e362936a3888e24ee9
Jude Baca-Miller <judeamiller@gmail.com>
2018-07-12 08:45:38 -0600

    Initial commit of snap concert.

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8"/>
+		<title>Persona for Concert Goers</title>
+	</head>
+	<body>
+		<h1>Persona: Steve Johnson</h1>
+		<img src="./stevejohnson.jpg" alt="photo of Steve">
+		<ol>
+			<li><strong>Age:</strong>58</li>
+			<li><strong>Gender:</strong>Male</li>
+			<li><strong>Occupation:</strong>Project Manager at a regional architecture and engineering firm.</li>
+			<li><strong>Attitude:</strong>Steve has spent the past 38 years of his life working in an office environment.
+				Jerry is comfortable using a desktop computer and regularly spends 6 hours per day in MS Project. Steve does
+				not use a smart phone much.</li>
+			<li><strong>Technology:</strong>Steve uses a 15" Inspiron Dell laptop running Windows 7 with a 2.5GHz i5 processor and
+					4GB of ram with a 1280 x 800 screen at home. At work Steve uses a Dell Optiplex tower with an i7 processor and
+					dual 24" 1080p monitors. Steve uses Chrome at work and at home, because his daughter uninstalled IE out
+					of frustration when playing the role of free tech support. He also owns an iPhone 5c running iOS 10 that
+					he inherited from his daughter. Steve only uses the iPhone for text messages and checking the weather.</li>
+
+		</ol>
+		<h2>Use Case</h2>
+		<p>Steve is in his late 50s and wants to relive his youth by attending more concerts. He is recently divorced, and
+			his daughter has recently moved to another city. Steve is lonely and needs to connect with like-minded individuals.
+			<br>
+			Steve has recently discovered that many of his favorite bands from his college days such as Journey and Rush
+			are frequently playing at local casinos. A quick google search informs Steve that this is happening all over the country.
+			Steve is excited to get back into the concert scene, and reconnect with	hip concert goers like himself.  Steve
+			wants to join a community where he can post about his concert experiences and let people he meets know which concert
+			he is going to next.</p>
+
+
+	</body>
+</html>
